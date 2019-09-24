e6540f8625d049b683bef772ab0970660a3da1bb
ARUNKUMAR <arunak283933@gmail.com>
2019-09-24 23:27:40 +0530

    final modification

--- src/com/struct/todo/app/LoginAction.java
+++ src/com/struct/todo/app/LoginAction.java
@@ -13,18 +13,8 @@ import org.apache.struts.action.ActionMapping;
 
 public class LoginAction extends org.apache.struts.action.Action {
 
-    /* forward name="success" path="" */
     private final static String SUCCESS = "success";
     private final static String FAILURE = "failure";
-    /**
-     * This is the action called from the Struts framework.
-     * @param mapping The ActionMapping used to select this instance.
-     * @param form The optional ActionForm bean for this request.
-     * @param request The HTTP Request we are processing.
-     * @param response The HTTP Response we are processing.
-     * @throws java.lang.Exception
-     * @return
-     */
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
